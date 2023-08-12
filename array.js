7553e8be1496260ce3f3cdb307601f231c5d1393
rootnure <nur.diu.2791@gmail.com>
2023-08-12 22:39:01 +0600

    Array is also a special type of object

---
+++ array.js
@@ -0,0 +1,18 @@
+/*
+const products = [
+    {name: 'phone', price: 12000},
+    {name: 'laptop', price: 32000}
+]
+
+products[0]
+products[1]
+*/
+
+// const ages = [21, 25, 27, 28, 26];
+const ages = {
+    '0': 21,
+    '1': 25,
+    '2': 27,
+    '3': 28,
+    '4': 26
+}
